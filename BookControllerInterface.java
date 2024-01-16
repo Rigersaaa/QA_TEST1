88f5dad732f58c15b25db89b958ef0d6fd020b79
TeaBegaj17 <128409954+TeaBegaj17@users.noreply.github.com>
2024-01-16 23:41:58 +0100

    Add files via upload

---
+++ BookControllerInterface.java
@@ -0,0 +1,11 @@
+package application.bookstore.models;
+
+public interface BookControllerInterface {
+        void setSaveListener();
+        void setSearchListener();
+        void setDefaultAuthor();
+        void setDeleteListener();
+        void setEditListener();
+        void resetFields();
+    }
+
